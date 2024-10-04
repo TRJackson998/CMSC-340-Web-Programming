9757c97a6219bbad06456f945ba15c1051172f07
TRJackson998 <85368064+TRJackson998@users.noreply.github.com>
2024-10-04 15:11:30 -0700

    server side 9 credit validation

--- Week_8/Assignment/Validate.php
+++ Week_8/Assignment/Validate.php
@@ -3,6 +3,15 @@
 
 function validate_NoMoreThan9CreditHours($avail_Reg_Courses)
 {
+  $total = 0;
+  foreach ($avail_Reg_Courses as $i) {
+    if ($i->getRegistered()) {
+      $total += $i->getCreditHours();
+    }
+  }
+  if ($total >= 9) {
+    return "You cannot register for more than 9 credit hours per term.";
+  }
   return "";
 }
 

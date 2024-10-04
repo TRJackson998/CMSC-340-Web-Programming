e34854b582fd76c87f55e77e0fb7d584ec8d7422
TRJackson998 <85368064+TRJackson998@users.noreply.github.com>
2024-10-04 14:03:42 -0700

    Formatting

--- Week_8/Assignment/Register-for-Courses.php
+++ Week_8/Assignment/Register-for-Courses.php
@@ -3,64 +3,55 @@ require_once 'login.php';
 require_once 'Course.php';
 require_once 'Validate.php';
 
-try
-{
+try {
   $pdo = new PDO($attr, $user, $pass, $opts);
-}
-catch (PDOException $e)
-{
-  throw new PDOException($e->getMessage(), (int)$e->getCode());
+} catch (PDOException $e) {
+  throw new PDOException($e->getMessage(), (int) $e->getCode());
 }
 
 // this if statement is executed only if the user selected a course before submitting
-if (isset($_POST['selectCourseElementName'])) 
-{
+if (isset($_POST['selectCourseElementName'])) {
 
-  $fail ="";
+  $fail = "";
   $selectCourseElementName = fix_string($_POST['selectCourseElementName']);
   //echo $selectCourseElementName . "<br>";
 
-  $query  = "SELECT * FROM w8_avail_Reg_Courses;";
+  $query = "SELECT * FROM w8_avail_Reg_Courses;";
   $result = $pdo->query($query);
 
   $avail_Reg_Courses = array();
   $i = 0;
-  while ($row = $result->fetch())
-  {
-    $avail_Reg_Courses[$i] = 
-    new Course($row['code'], $row['title'], $row['creditHours'], $row['registered']);
+  while ($row = $result->fetch()) {
+    $avail_Reg_Courses[$i] =
+      new Course($row['code'], $row['title'], $row['creditHours'], $row['registered']);
     $i++;
   }
 
-  $fail  = validate_NoMoreThan9CreditHours($avail_Reg_Courses);
+  $fail = validate_NoMoreThan9CreditHours($avail_Reg_Courses);
 
-  $fail  .= validate_NotPreviouslyRegistered($avail_Reg_Courses, $selectCourseElementName);
+  $fail .= validate_NotPreviouslyRegistered($avail_Reg_Courses, $selectCourseElementName);
 
   //if validation is OK, set registered to true (course is registered for)
-  if ($fail == "") 
-  {
-    $query  = "UPDATE w8_avail_Reg_Courses SET registered = TRUE WHERE code = '$selectCourseElementName';";
+  if ($fail == "") {
+    $query = "UPDATE w8_avail_Reg_Courses SET registered = TRUE WHERE code = '$selectCourseElementName';";
     $result = $pdo->query($query);
 
-  } 
-  else
-  {
+  } else {
     echo "<span style='color:red'>Server-side Validation Errors = <br>$fail</span><br>";
   }
 }
 
 // This code is executed every time this php file is requested
 // get a fresh list of available courses and a fresh list of registered courses
-$query  = "SELECT * FROM w8_avail_Reg_Courses;";
+$query = "SELECT * FROM w8_avail_Reg_Courses;";
 $result = $pdo->query($query);
 
 $avail_Reg_Courses = array();
 $i = 0;
-while ($row = $result->fetch())
-{
-$avail_Reg_Courses[$i] = 
-new Course($row['code'], $row['title'], $row['creditHours'], $row['registered']);
-$i++;
+while ($row = $result->fetch()) {
+  $avail_Reg_Courses[$i] =
+    new Course($row['code'], $row['title'], $row['creditHours'], $row['registered']);
+  $i++;
 }
 
 echo <<<_END
@@ -77,27 +68,25 @@ echo <<<_END
     <p>
     Select a course from these available courses for registration:
     </p>
-    <form method="post" action="Register-for-Courses.php" 
+    <form method="post" action="Register-for-Courses.php"
       onSubmit="return validate(this)">
     <select id="selectCourseElementID" name="selectCourseElementName" size="5">
 _END;
 
 $currentRegisterdCourses = "";
 $currentCreditHours = 0;
-foreach ($avail_Reg_Courses as $course) 
-{
-    $code = $course->getCode();
-    $title = $course->getTitle();
-    $creditHours = $course->getCreditHours();
-    $registered = $course->getRegistered();
+foreach ($avail_Reg_Courses as $course) {
+  $code = $course->getCode();
+  $title = $course->getTitle();
+  $creditHours = $course->getCreditHours();
+  $registered = $course->getRegistered();
 
-    echo "<option value='$code'>$code $title</option>";
+  echo "<option value='$code'>$code $title</option>";
 
-    if($registered == true) 
-    {
+  if ($registered == true) {
     $currentRegisterdCourses = $currentRegisterdCourses . $code . " " . $title . "<br>";
     $currentCreditHours = $currentCreditHours + $creditHours;
-    }
+  }
 
 }
 
@@ -129,8 +118,8 @@ _END;
 
 function fix_string($string)
 {
-  if (get_magic_quotes_gpc()) $string = stripslashes($string);
-    return htmlentities ($string);
+  $string = stripslashes($string);
+  return htmlentities($string);
 }
 
 ?>
--- Week_8/Assignment/Validate.js
+++ Week_8/Assignment/Validate.js
@@ -1,24 +1,19 @@
-function validate(form)
-{
+function validate(form) {
   fail = "";
-  fail  = validate_NoMoreThan9CreditHours();
+  fail = validate_NoMoreThan9CreditHours();
   fail += validate_NotPreviouslyRegistered();
 
-  if (fail == "")
-    return true;
-  else 
-  { 
-    alert("Client-side Validation Errors = \n" + fail); 
+  if (fail == "") return true;
+  else {
+    alert("Client-side Validation Errors = \n" + fail);
     return false;
   }
 }
 
-function validate_NoMoreThan9CreditHours()
-{
+function validate_NoMoreThan9CreditHours() {
   return "";
 }
 
-function validate_NotPreviouslyRegistered()
-{
+function validate_NotPreviouslyRegistered() {
   return "";
 }

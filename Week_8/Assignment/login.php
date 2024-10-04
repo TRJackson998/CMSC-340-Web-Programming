f2eceee0165ec77bea7799b9bc14ee0154f1819d
TRJackson998 <85368064+TRJackson998@users.noreply.github.com>
2024-10-04 14:00:41 -0700

    Update port to fix connection

--- Week_8/Assignment/login.php
+++ Week_8/Assignment/login.php
@@ -1,15 +1,15 @@
 <?php // login.php
 $host = 'localhost';    // Change as necessary
-$port = '3305';
+$port = '3306';
 $data = 'cmsc340'; // Change as necessary
 $user = 'root';         // Change as necessary
 $pass = 'mysql';        // Change as necessary
 $chrs = 'utf8mb4';
 $attr = "mysql:host=$host;port=$port;dbname=$data;charset=$chrs";
 $opts =
-[
- PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
- PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
- PDO::ATTR_EMULATE_PREPARES => false,
-];
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
 ?>

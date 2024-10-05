e902b8490d23363d07f72b9d60f612790fa3ebe4
TRJackson998 <85368064+TRJackson998@users.noreply.github.com>
2024-10-05 10:18:49 -0700

    Client side already reg validation

--- Week_8/Assignment/Validate.js
+++ Week_8/Assignment/Validate.js
@@ -19,5 +19,23 @@ function validate_NoMoreThan9CreditHours() {
 }
 
 function validate_NotPreviouslyRegistered() {
+  // Get the text from the select element to find the selected course
+  select = document.getElementById("selectCourseElementID");
+  selectIndex = select.selectedIndex; // find the index of the selected element
+  selectedCourse = select.options.item(selectIndex).text; // pull out the text
+
+  // Get the text from the td element listing registered courses and turn it into an array
+  selectedCourses = document.getElementById("currentCoursesElementID");
+  selectedCoursesText = selectedCourses.innerHTML; // get the HTML text of the element
+  selectedCoursesText = selectedCoursesText.replace(/<*td>/, ""); // remove outer td tags
+  selectedCoursesArray = selectedCoursesText.split("<br>"); // split elements on br tags
+
+  // Loop over the array to find out whether already registered
+  for (i = 0; i < selectedCoursesArray.length; i++) {
+    loopCourse = selectedCoursesArray[i];
+    if (selectedCourse == loopCourse) {
+      return "You are already registered for the " + selectedCourse;
+    }
+  }
   return "";
 }
